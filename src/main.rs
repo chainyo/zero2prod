04dd1246923264caf94988d811e6f415c556faeb
chainyo <t.chaigneau.tc@gmail.com>
2023-06-19 21:08:45 +0200

    run quality pipeline

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    zero2prod::run(listener).expect("Failed to bind address").await
+    zero2prod::run(listener)
+        .expect("Failed to bind address")
+        .await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,4 @@
+#![allow(clippy::all)]
 use std::net::TcpListener;
 
 // Launch our application in the background ~somehow~
